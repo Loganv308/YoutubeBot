46bbf7ed799d19460c5d5c3e12316765e01a9de7
Loganv308 <loganv308@gmail.com>
2022-12-07 20:11:53 -0600

    Implementing streaming

--- client/client.js
+++ client/client.js
@@ -1,13 +1,18 @@
-import { Client, Collection, GatewayIntentBits } from 'discord.js';
+"use strict";
 
-export default class extends Client {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _discord = require("discord.js");
+class _default extends _discord.Client {
   constructor(config) {
     super({
-      intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+      intents: [_discord.GatewayIntentBits.Guilds, _discord.GatewayIntentBits.GuildVoiceStates, _discord.GatewayIntentBits.GuildMessages, _discord.GatewayIntentBits.MessageContent]
     });
-
-    this.commands = new Collection();
-
+    this.commands = new _discord.Collection();
     this.config = config;
   }
-};
+}
+exports.default = _default;
+;
